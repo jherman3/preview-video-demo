e301d69ca2863dca4607c9be8c4055ecc077f505
Jake Herman <jakeherman3@gmail.com>
2014-06-25 09:35:48 -0700

    Minor cleanup

--- preview.pl
+++ preview.pl
@@ -1,10 +1,21 @@
 #!/usr/bin/perl
+if(@ARGV != 1) {
+    print "Usage: ./preview <VIDEO_URL>\n";
+    exit 1;
+}
+
+system("hash preview");
+if($? != 0) {
+    print "Error: The preview binary must be on your PATH\n";
+    exit 2;
+}
 
 my $url = $ARGV[0];
 my $loc = `preview renderV2 --verbose --verbose localhost:8080 --template video $url 2>&1`;
 
 $loc =~ /{"Key":"streamingUrl","Value":\["(.+)"\]/;
 $loc = $1;
+print "Writing URL $loc to source.json\n";
 open(my $fh, '>', "source.json");
 print $fh '{"source":"' . $loc . '"}';
 close($fh);
